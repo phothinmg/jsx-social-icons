9b671402ef069549e5bcc2625aadcd829c536e61
phothinmg <phothinmg@gmail.com>
2024-07-23 19:35:18 +0630

    modified:   .gitignore

--- src/react/index.tsx
+++ src/react/index.tsx
@@ -16,68 +16,68 @@ import type { JsxSocialIconProps } from "../types";
  * @returns A JSX element representing the social media icon.
  */
 const JsxSocialIcon = ({
-  name,
-  href = "#",
-  target = "_blank",
-  fillColor = "currentcolor",
-  fillOpacity = 0.7,
-  className = "",
-  size = 24,
+	name,
+	href = "#",
+	target = "_blank",
+	fillColor = "currentcolor",
+	fillOpacity = 0.7,
+	className = "",
+	size = 24,
 }: JsxSocialIconProps): JSX.Element => {
-  const [pathd, setPathd] = useState<string>(() => getPathd(name));
-  const [opacity, setOpacity] = useState<number>(fillOpacity);
-  const [color, setColor] = useState<string>(fillColor);
-  const [link, setLink] = useState<string>(href);
-  const [tg, setTg] = useState<string>(target);
-  const [cname, setCname] = useState<string>(className);
-  const [sz, setSz] = useState<number>(size);
+	const [pathd, setPathd] = useState<string>(() => getPathd(name));
+	const [opacity, setOpacity] = useState<number>(fillOpacity);
+	const [color, setColor] = useState<string>(fillColor);
+	const [link, setLink] = useState<string>(href);
+	const [tg, setTg] = useState<string>(target);
+	const [cname, setCname] = useState<string>(className);
+	const [sz, setSz] = useState<number>(size);
 
-  useEffect(() => {
-    setPathd(getPathd(name));
-  }, [name]);
+	useEffect(() => {
+		setPathd(getPathd(name));
+	}, [name]);
 
-  useEffect(() => {
-    setOpacity(fillOpacity);
-  }, [fillOpacity]);
+	useEffect(() => {
+		setOpacity(fillOpacity);
+	}, [fillOpacity]);
 
-  useEffect(() => {
-    setColor(fillColor);
-  }, [fillColor]);
+	useEffect(() => {
+		setColor(fillColor);
+	}, [fillColor]);
 
-  useEffect(() => {
-    setLink(href);
-  }, [href]);
+	useEffect(() => {
+		setLink(href);
+	}, [href]);
 
-  useEffect(() => {
-    setTg(target);
-  }, [target]);
+	useEffect(() => {
+		setTg(target);
+	}, [target]);
 
-  useEffect(() => {
-    setCname(className);
-  }, [className]);
+	useEffect(() => {
+		setCname(className);
+	}, [className]);
 
-  useEffect(() => {
-    setSz(size);
-  }, [size]);
+	useEffect(() => {
+		setSz(size);
+	}, [size]);
 
-  return (
-    <a
-      href={link}
-      style={{ textDecoration: "none", color: "currentcolor" }}
-      className={cname}
-      target={tg}
-    >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
-        width={sz}
-        height={sz}
-      >
-        <title>Jsx Social Icon</title>
-        <path d={pathd} fill={color} fillOpacity={opacity} />
-      </svg>
-    </a>
-  );
+	return (
+		<a
+			href={link}
+			style={{ textDecoration: "none", color: "currentcolor" }}
+			className={cname}
+			target={tg}
+		>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				viewBox="0 0 24 24"
+				width={sz}
+				height={sz}
+			>
+				<title>Jsx Social Icon</title>
+				<path d={pathd} fill={color} fillOpacity={opacity} />
+			</svg>
+		</a>
+	);
 };
 
 export default JsxSocialIcon;
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,40 +1,40 @@
 export interface Icons {
-  name:
-    | ""
-    | "github"
-    | "blogger"
-    | "gitlab"
-    | "gitbook"
-    | "codepen"
-    | "codesandbox"
-    | "hashnode"
-    | "twitter-X"
-    | "facebook"
-    | "discord"
-    | "disroot"
-    | "linkedin"
-    | "line"
-    | "dev-to"
-    | "medium"
-    | "slack"
-    | "stackoverflow"
-    | "stack-edit"
-    | "messenger"
-    | "viber"
-    | "telegram"
-    | "whatsapp"
-    | "wechat"
-    | "youtube"
-    | "wordpress";
-  d: string;
+	name:
+		| ""
+		| "github"
+		| "blogger"
+		| "gitlab"
+		| "gitbook"
+		| "codepen"
+		| "codesandbox"
+		| "hashnode"
+		| "twitter-X"
+		| "facebook"
+		| "discord"
+		| "disroot"
+		| "linkedin"
+		| "line"
+		| "dev-to"
+		| "medium"
+		| "slack"
+		| "stackoverflow"
+		| "stack-edit"
+		| "messenger"
+		| "viber"
+		| "telegram"
+		| "whatsapp"
+		| "wechat"
+		| "youtube"
+		| "wordpress";
+	d: string;
 }
 
 export interface JsxSocialIconProps {
-  name: Icons["name"];
-  href?: string;
-  target?: "_blank" | "_parent" | "_self" | "_top";
-  className?: string;
-  size?: number;
-  fillColor?: string;
-  fillOpacity?: 0.1 | 0.2 | 0.3 | 0.4 | 0.5 | 0.6 | 0.7 | 0.8 | 0.9 | 1;
+	name: Icons["name"];
+	href?: string;
+	target?: "_blank" | "_parent" | "_self" | "_top";
+	className?: string;
+	size?: number;
+	fillColor?: string;
+	fillOpacity?: 0.1 | 0.2 | 0.3 | 0.4 | 0.5 | 0.6 | 0.7 | 0.8 | 0.9 | 1;
 }
